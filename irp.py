e84b4f71375494b67d538057fb468dd3d0e00acc
rpj <wasmus@zom.bi>
2019-07-11 14:07:21 +0200

    add handshake; simple messaging

--- irp.py
+++ irp.py
@@ -2,15 +2,33 @@
 
 # server for js impress remote
 
-from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
+import json
 from collections import OrderedDict
+from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
 
 class IRPApp (WebSocketApplication):
     def on_open (self):
+        self.connected = False
         print("new conn")
 
     def on_message (self, message):
-        print("msg", message)
+
+        # handshake
+        if 'hello' == message:
+            self.ws.send('hello')
+            self.connected = True
+
+        elif None == message:
+            pass # disconnected
+
+        else:
+            print(message)
+            data = json.loads(message)
+            self.irp_msg(data)
+
+    def irp_msg (self, msg):
+        action = msg["action"]
+        print(action)
 
     def on_close (self, reason):
         print("close", reason)
